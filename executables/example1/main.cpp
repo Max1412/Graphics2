3dc2893e0f364903a572ea9e42aa7a1b7a44f457
Maximilian Mader <mmader@uni-koblenz.de>
2017-06-30 02:00:58 +0200

    Finish first example in main

--- executables/example1/main.cpp
+++ executables/example1/main.cpp
@@ -12,6 +12,9 @@
 #include <sstream>
 
 #include "Utils/UtilCollection.h"
+#include "Rendering/Shader.h"
+#include "Rendering/ShaderProgram.h"
+#include "Rendering/Buffer.h"
 
 std::string GLubyteToString(const GLubyte* content) {
 	return std::string(reinterpret_cast<const char*>(content));
@@ -31,11 +34,52 @@ int main(int argc, char* argv[]) {
 	// get list of OpenGL extensions (can be searched later if needed)
 	std::vector<std::string> extensions = util::getGLExtenstions();
 
-	
+	Shader vs("basic.vert", GL_VERTEX_SHADER);
+	Shader fs("basic.frag", GL_FRAGMENT_SHADER);
+	ShaderProgram sp(vs, fs);
+	sp.use();
+
+
+
+	std::array<float, 9> positionData = {
+		-0.8f, -0.8f, 0.0f,
+		0.8f, -0.8f, 0.0f,
+		0.0f, 0.8f, 0.0f };
+
+	std::array<float, 9>  colorData = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f };
+
+	Buffer positionBuffer;
+	positionBuffer.setData(positionData, GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+
+	Buffer colorBuffer;
+	colorBuffer.setData(colorData, GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+
+	GLuint vaoHandle;
+
+	// Create and set-up the vertex array object
+	glGenVertexArrays(1, &vaoHandle);
+	glBindVertexArray(vaoHandle);
+	// Enable the vertex attribute arrays
+	glEnableVertexAttribArray(0); // Vertex position
+	glEnableVertexAttribArray(1); // Vertex color
+								  // Map index 0 to the position buffer
+	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer.getHandle());
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+	// Map index 1 to the color buffer
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer.getHandle());
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
+
+	glBindVertexArray(vaoHandle);
+
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
 
-
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
 	}
 
 	// close window
