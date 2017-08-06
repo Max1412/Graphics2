91749dbaee0849673e701722e3ac5625b547a64f
Maximilian Mader <mmader@uni-koblenz.de>
2017-08-06 11:31:42 +0200

    Add light position slider

--- executables/demo1/main.cpp
+++ executables/demo1/main.cpp
@@ -282,6 +282,13 @@ int main(int argc, char* argv[]) {
                     size_t spotDirOffset = i * sizeof(lvec.at(i)) + offsetof(LightInfo, spot_direction);
                     lightBuffer.setContentSubData(lvec.at(i).spot_direction, spotDirOffset);
                 }
+                // maps memory to access it by GUI -- probably very bad performance-wise
+                size_t positionOffset = i * sizeof(lvec.at(i));
+                lightBuffer.bind();
+                float* ptr = static_cast<float*>(glMapBufferRange(GL_SHADER_STORAGE_BUFFER, positionOffset, sizeof(glm::vec3), GL_MAP_READ_BIT | GL_MAP_WRITE_BIT));
+                ImGui::SliderFloat3((std::string("Position ") + n.str()).c_str(), ptr, -30.0f, 30.0f);
+                glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+
 
             }
             ImGui::End();
--- executables/demo2/main.cpp
+++ executables/demo2/main.cpp
@@ -198,6 +198,12 @@ int main(int argc, char* argv[]) {
                     std::cout << spotDirOffset << " " << spotDirOffset2 << std::endl;
                     lightBuffer.setContentSubData(lvec.at(i).spot_direction, spotDirOffset);
                 }
+                // maps memory to access it by GUI -- probably very bad performance-wise
+                size_t positionOffset = i * sizeof(lvec.at(i));
+                lightBuffer.bind();
+                float* ptr = static_cast<float*>(glMapBufferRange(GL_SHADER_STORAGE_BUFFER, positionOffset, sizeof(glm::vec3), GL_MAP_READ_BIT | GL_MAP_WRITE_BIT));
+                ImGui::SliderFloat3((std::string("Position ") + n.str()).c_str(), ptr, -30.0f, 30.0f);
+                glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 
             }
             ImGui::End();
