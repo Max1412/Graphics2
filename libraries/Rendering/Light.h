9f8eff067b19a3d73a282d9a6275cd74b1a5b48f
Maximilian Mader <mmader@uni-koblenz.de>
2018-05-14 17:12:42 +0200

    Put LightManager in a seperate file

--- libraries/Rendering/Light.h
+++ libraries/Rendering/Light.h
@@ -99,32 +99,3 @@ private:
 
     GPULight m_gpuLight;
 };
-
-class LightManager
-{
-public:
-
-    LightManager();
-    explicit LightManager(std::vector<std::shared_ptr<Light>> lights);
-
-    void uploadLightsToGPU();
-
-    bool showLightGUIs();
-
-	bool showLightGUIsContent();
-
-    void renderShadowMaps(const std::vector<std::shared_ptr<Mesh>>& scene);
-    void updateLightParams();
-    void updateLightParams(std::shared_ptr<Light> light);
-
-    void addLight(std::shared_ptr<Light> light);
-
-    std::vector<std::shared_ptr<Light>> getLights() const;
-
-private:
-
-    Buffer m_lightsBuffer{ GL_SHADER_STORAGE_BUFFER };
-    std::vector<std::shared_ptr<Light>> m_lightList;
-    int m_e = 0;
-
-};
