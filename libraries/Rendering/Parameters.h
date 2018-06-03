b8477cfdf74c8c74d9b9dccaffdd2d5e6996a325
Darius Thies <dath@uni-koblenz.de>
2018-06-03 14:15:19 +0200

    added paramters for breakfast room scene

--- libraries/Rendering/Parameters.h
+++ libraries/Rendering/Parameters.h
@@ -75,3 +75,26 @@ Parameters sponza{
 	2.2f, 0.1f,
 	3000.0f
 };
+
+Parameters breakfast{
+	{
+		//global directional light
+		{ glm::vec3(4.0f), glm::vec3(12.0f, 10.0f, 0.0f), glm::vec3(-1.0f, -1.0f, -0.2f), 2 },
+		//lamp 1
+		{ glm::vec3(2.0f, 2.0f, 1.3f), glm::vec3(1.0f, 4.15f, -1.92f), glm::vec3(0.001f, -1.0f, 0.0f), 1,
+		0.025f, 0.01f, 0.0f, 1.055f, 0.72f },
+		//lamp 2
+		{ glm::vec3(2.0f, 2.0f, 1.3f), glm::vec3(-2.15f, 4.15f, -1.92f), glm::vec3(0.001f, -1.0f, 0.0f), 1,
+		0.025f, 0.01f, 0.0f, 1.055f, 0.72f },
+		//lamp 3 as sun
+		{ glm::vec3(10.0f), glm::vec3(15.0f, 10.0f, -1.0f), glm::vec3(-0.6f, -0.33f, 0.0f), 3,
+		0.025f, 0.01f, 0.0f, 1.0f, 0.92f },
+	},
+	//noise
+	{ 0.015f, 0.1f, 0.5f, 0.3f },
+	//fog
+	{ glm::vec3(1.0f), 0.35f, 0.1f, 0.1f, 0.15f },
+	//hdr
+	2.2f, 0.15f,
+	30.0f
+};
